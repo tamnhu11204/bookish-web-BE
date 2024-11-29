0bdf9a6a5cbccd0ef69e7ced01709c4dff5b3b4a
tamnhu11204 <22521053@gm.uit.edu.vn>
2024-11-29 14:28:47 +0700

    login

--- src/routes/UserRouter.js
+++ src/routes/UserRouter.js
@@ -2,6 +2,7 @@ const express =require ("express")
 const router= express.Router()
 const userController=require('../controllers/UserController')
 
-router.post('/', userController.createUser)
+router.post('/signup', userController.createUser)
+router.post('/login', userController.loginUser)
 
 module.exports=router
--- src/services/UserService.js
+++ src/services/UserService.js
@@ -1,14 +1,25 @@
 const User = require('../models/UserModel');
+const bcrypt=require ("bcrypt")
 
 const createUser = (newUser) => {
     return new Promise(async (resolve, reject) => {
+        const { email, name, password, phone, img, birthday, active, isAdmin, gender } = newUser;
         try {
-            const { email, name, password, phone, img, birthday, active, isAdmin, gender } = newUser;
-
+            const checkUser = await User.findOne({
+                email: email
+            })
+            if (checkUser !== null) {
+                resolve({
+                    status: 'OK',
+                    message: 'The email is already'
+                })
+            }
+            const hash = bcrypt.hashSync(password, 10)
+            console.log('hash', hash)
             const createdUser = await User.create({
                 email,
                 name,
-                password,
+                password: hash,
                 phone,
                 img,
                 birthday,
@@ -16,17 +27,11 @@ const createUser = (newUser) => {
                 isAdmin,
                 gender
             });
-
             if (createdUser) {
                 resolve({
                     status: 'OK',
                     message: 'User created successfully',
                     data: createdUser
-                });
-            } else {
-                resolve({
-                    status: 'ERR',
-                    message: 'Failed to create user'
                 });
             }
         } catch (e) {
@@ -35,4 +40,37 @@ const createUser = (newUser) => {
     });
 };
 
-module.exports = { createUser };
+const loginUser = (userLogin) => {
+    return new Promise(async (resolve, reject) => {
+        const { email, name, password, phone, img, birthday, active, isAdmin, gender } = userLogin;
+        try {
+            const checkUser = await User.findOne({
+                email: email
+            })
+            if (checkUser === null) {
+                resolve({
+                    status: 'OK',
+                    message: 'The user is not defined'
+                })
+            }
+            const comparePassword=bcrypt.compareSync(password,checkUser.password)
+            console.log('comparePassword', comparePassword)
+            if (!comparePassword) {
+                resolve({
+                    status: 'OK',
+                    message: 'The password or user is incorrect',
+                    data: createdUser
+                });
+            }
+            resolve({
+                status: 'OK',
+                message: 'Success',
+                data: checkUser
+            });
+        } catch (e) {
+            reject(e);
+        }
+    });
+};
+
+module.exports = { createUser, loginUser };
