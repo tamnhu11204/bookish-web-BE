8be81ab07c5242a23f817588a83f6eee6ac5a9d5
pn3005 <22520939@gm.uit.edu.vn>
2025-01-05 20:52:37 +0700

    delete  product

--- src/routes/ProductRouter.js
+++ src/routes/ProductRouter.js
@@ -6,7 +6,7 @@ const { authMiddleWare } = require("../middleware/authMiddleware")
 router.post('/create', productController.createProduct)
 router.put('/update/:id', authMiddleWare, productController.updateProduct)
 router.get('/get-detail/:id',productController.getDetailProduct)
-router.delete('/delete/:id',authMiddleWare, productController.deleteProduct)
+router.delete('/delete/:id', productController.deleteProduct)
 router.get('/get-all',productController.getAllProduct)
 
 module.exports=router
